b521e0d3dfea044a5e423ab86d1509a4b1d89033
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-05-06 21:38:34 +0530

    Quick_sort algo using devide conquar algo

---
+++ Quick_sort.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+
+int pertition(int arr[],int low,int high){
+    int pivot=arr[high];
+    int i=low-1;
+    for(int j=low;j<high;j++){
+        if(arr[j]<=pivot){
+            i++;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[high]);
+    return i+1;
+}
+
+void Quick_sort(int arr[],int low,int high){
+    if(low<high){
+        int k=pertition(arr,low,high);
+        Quick_sort(arr,low,k-1);
+        Quick_sort(arr,k+1,high);
+    }
+}
+
+void display(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter num of element in array ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<i+1<<" element"<<" ";
+        cin>>arr[i];
+    }
+    cout<<"UNSORTED ARRAY --->"<<" ";
+    display(arr,n);
+    cout<<"SORTED ARRAY --->"<<" ";
+    Quick_sort(arr,0,n-1);
+    display(arr,n);
+}
