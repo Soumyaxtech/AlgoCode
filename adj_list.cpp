e177731ce93d7226cf44d389d6b9aa22e87fba1c
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-05-23 11:16:34 +0530

    graph representation of adjacency list

--- adj_list.cpp
+++ adj_list.cpp
@@ -5,5 +5,27 @@ int main(){
     ifstream x("temp.txt");
     int n;
     x>>n;
-    vector<vector<int>>
+    vector<vector<int>>adjlist(n);
+    int a[100][100];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            x>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+           if(a[i][j]==1){
+            adjlist[i].push_back(j);
+           }
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<i<<":";
+        for(int j:adjlist[i]){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    x.close();
+    return 0;
 }
