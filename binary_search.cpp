ed916090a8896c0846728c00a168b29914dc81b0
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-06-03 09:13:54 +0530

    binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -7,14 +7,17 @@ int binarysearch(int arr[], int n, int k) {
         int high=n-1;
         while(low<=high){
             int mid=(high+low)/2;
+            cout << "Low: " << low << ", High: " << high << ", Mid: " << mid << ", arr[Mid]: " << arr[mid] << endl;
             if(arr[mid]==k){
                 return mid;
             }
             else if(k>arr[mid]){
                 low=mid+1;
+                
             }
             else{
                 high=mid-1;
+                
             }
         }
         return -1;
